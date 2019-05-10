26f57621fd122e8746a3aacd60c9a5b21c524358
hubtorhazar <malthehermansen@hotmail.com>
2019-05-10 11:16:20 +0200

    Småting

---
+++ Version2 Rock App/ViewModel/RockViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version2_Rock_App.ViewModel
+{
+    class RockViewModel : INotifyPropertyChanged
+    {
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged
+            ([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new
+                PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
