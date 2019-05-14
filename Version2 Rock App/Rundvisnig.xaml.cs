946f6d9f7012d424074df22539d5ffd1f12032c7
Konrad Schantz Kirkebjerg <konr0693@edu.easj.dk>
2019-05-14 09:22:07 +0200

    visning

--- Version2 Rock App/Rundvisnig.xaml.cs
+++ Version2 Rock App/Rundvisnig.xaml.cs
@@ -20,9 +20,9 @@ namespace Version2_Rock_App
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Rundvisning : Page
     {
-        public MainPage()
+        public Rundvisning()
         {
             this.InitializeComponent();
         }
