7e1f6d31e52c35d535fdf7b7d6609227e6ad4029
Konrad Schantz Kirkebjerg <konr0693@edu.easj.dk>
2019-05-20 20:32:39 +0200

    Tilføjet artefakter classe, tilføjet tour xaml

---
+++ Version2 Rock App/Model/Artefakter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Version2_Rock_App;
+
+namespace Version2_Rock_App.Model
+{
+
+    
+
+    public class Artefakter
+    {
+        private int _aId;
+        private string _name;
+        private int _year;
+        private string _description;
+        private string _pic;
+
+
+        public Artefakter(int aId, string name, int year, string description, string pic)
+        {
+            _aId = aId;
+            _name = name;
+            _year = year;
+            _description = description;
+            _pic = pic;
+        }
+
+        public int aId
+        {
+            get { return aId;}
+            set { _aId = value; }
+        }
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public string description
+        {
+            get { return _description;}
+            set { _description = value; }
+        }
+
+        public string pic
+        {
+            get { return _pic; }
+            set { _pic = value; }
+        }
+
+    }
+    
+    
+
+
+
+}
