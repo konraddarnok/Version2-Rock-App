5d8279070390f37537e580e497cab777fa6e6138
Jan Selck Hansen <janx3876@edu.easj.dk>
2019-05-21 10:38:10 +0200

    tilføjet genreklasse

---
+++ Version2 Rock App/Model/genre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Version2_Rock_App.Model
+{
+    class genre
+    {
+        private string _pop;
+        private string _rock;
+        private string _heavyMetal;
+
+        public Genre (string pop, string rock, string heavyMetal)
+        {
+            Pop = pop;
+            Rock = rock;
+            HeavyMetal = heavyMetal;
+            
+        }
+        public string Pop { get; set; }
+        public string Rock { get; set; }
+        public string HeavyMetal { get; set; }
+        
+    }
+
+}
