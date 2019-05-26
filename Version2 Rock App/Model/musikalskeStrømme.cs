a2d4aded370e621b1c667611de270c3013cdc544
Jan Selck Hansen <janx3876@edu.easj.dk>
2019-05-26 11:52:41 +0200

    musikalske stømme

---
+++ Version2 Rock App/Model/musikalskeStrømme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Version2_Rock_App.Model
+{
+    class Musikalske_strømme
+    {
+        private string _aarti;
+        private string _sang;
+        private string _kunstner;
+
+        public Musikalske_strømme(string aarti, string sang, string kunstner)
+        {
+            Aarti = aarti;
+            Sang = sang;
+            Kunstner = kunstner;
+           }
+        public string Aarti { get; set; }
+        public string Sang { get; set; }
+        public string Kunstner { get; set; }
+    }
+}
